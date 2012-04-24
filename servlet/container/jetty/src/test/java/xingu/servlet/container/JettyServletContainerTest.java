177bd73435b143b7f8ce757bc922e31335c27a15
Leandro Cruz <leandro.saad@gmail.com>
2012-04-24 12:48:19 -0300

    importing test code from unicorn-commons

---
+++ servlet/container/jetty/src/test/java/xingu/servlet/container/JettyServletContainerTest.java
@@ -0,0 +1,78 @@
+package xingu.servlet.container;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import xingu.servlet.container.ApplicationContext;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import xingu.servlet.container.ServletContainer;
+
+import br.com.ibnetwork.xingu.container.Inject;
+import br.com.ibnetwork.xingu.container.XinguTestCase;
+
+@Ignore
+public class JettyServletContainerTest
+    extends XinguTestCase
+{
+    @Inject
+    private ServletContainer container;
+    
+    
+    @Override
+    protected String getContainerFile()
+    {
+        return "pulga-servlet.xml";
+    }
+
+    @Test
+    public void testContextRestart()
+        throws Exception
+    {
+        ApplicationContext ctx = container.context("/test");
+        assertTrue(ctx.isRunning());
+
+        URL url = new URL("http://localhost:9192/test");
+        assertEquals("Test.Index", read(url));
+        
+        ctx.stop();
+        assertFalse(ctx.isRunning());
+        try
+        {
+            read(url);
+            fail("Should have thrown exception");
+        }
+        catch(FileNotFoundException e)
+        {}
+        
+        ctx.start();
+        assertTrue(ctx.isRunning());
+        assertEquals("Test.Index", read(url));
+    }
+
+    public String read(URL url)
+        throws Exception
+    {
+        StringBuffer sb = new StringBuffer();
+        URLConnection yc = url.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+        String inputLine;
+
+        while ((inputLine = in.readLine()) != null)
+        {
+            sb.append(inputLine);
+        }
+        in.close();
+        return sb.toString();
+    }
+}
