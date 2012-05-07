f53b0efdebe6196378f49f27537c63aa1ce2c7ea
Leandro Cruz <leandro.saad@gmail.com>
2012-05-07 17:15:08 -0300

    adding ids to Command and CommandReply

--- servlet/command/src/main/java/xingu/servlet/command/Command.java
+++ servlet/command/src/main/java/xingu/servlet/command/Command.java
@@ -2,11 +2,13 @@ package xingu.servlet.command;
 
 import java.io.Serializable;
 
+import br.com.ibnetwork.xingu.lang.WithId;
+
 /**
  * Don't remove this interface. This is required to speed-up the gwt compilation process
  *   
  * @see http://efreedom.com/Question/1-3059787/Gwt-Using-List-Serializable-RPC-Call
  */
 public interface Command
-    extends Serializable
+    extends Serializable, WithId
 {}
--- servlet/command/src/main/java/xingu/servlet/command/CommandReplySupport.java
+++ servlet/command/src/main/java/xingu/servlet/command/CommandReplySupport.java
@@ -1,13 +1,28 @@
 package xingu.servlet.command;
 
+
 public class CommandReplySupport
     implements CommandReply
 {
     protected String result = OK; /* used in json serialziation. Do not remove */
+	
+    private long id;
     
     @Override
     public String result()
     {
         return result;
     }
+
+	@Override
+	public void setId(long id)
+	{
+		this.id = id;
+	}
+
+	@Override
+	public long getId()
+	{
+		return id;
+	}
 }
