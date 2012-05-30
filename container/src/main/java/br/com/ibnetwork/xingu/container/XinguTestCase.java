51fd29f8a3afe21460f45c0d6d58c98f0179428a
Leandro Cruz <leandro.saad@gmail.com>
2012-05-30 10:27:52 -0300

    added resetAllMocks() method

--- container/src/main/java/br/com/ibnetwork/xingu/container/XinguTestCase.java
+++ container/src/main/java/br/com/ibnetwork/xingu/container/XinguTestCase.java
@@ -1,13 +1,23 @@
 package br.com.ibnetwork.xingu.container;
 
+import static org.mockito.Mockito.mock;
+
+import java.util.List;
+
 import org.junit.AfterClass;
 import org.junit.Before;
+import org.mockito.internal.util.MockUtil;
+import static org.mockito.Mockito.reset;
 
 import br.com.ibnetwork.xingu.utils.FSUtils;
 
 public class XinguTestCase 
 {
 	private static Container container;
+	
+	private static MockUtil mockUtil = new MockUtil();
+	
+	private static Binder binder;
 
     protected Container getContainer()
     	throws Exception
@@ -45,10 +55,11 @@ public class XinguTestCase
 	    if(container == null)
 	    {
             container = getContainer();
-            Binder binder = container.lookup(Binder.class);
+            binder = container.lookup(Binder.class);
             rebind(binder);
 	    }
 	    injectDependencies();
+	    resetAllMocks();
 	}
     
 	protected void rebind(Binder binder)
@@ -60,7 +71,33 @@ public class XinguTestCase
 	    Injector injector = container.lookup(Injector.class);
 	    injector.injectDependencies(this);
 	}
+    
+    protected <T> void withMock(Class<T> clazz)
+	{
+    	T impl = mock(clazz);
+    	binder.bind(clazz).to(impl);
+	}
 
+    protected void resetAllMocks()
+	{
+    	List<Class<?>> roles = binder.getRoles();
+    	for (Class<?> role : roles)
+		{
+    		Binding<?> binding = binder.get(role);
+    		if(binding == null)
+    		{
+    			continue;
+    		}
+    		
+    		Object impl = binding.impl();
+    		if(mockUtil.isMock(impl))
+    		{
+    			reset(impl);
+    		}
+		}
+	}
+
+    
     @AfterClass
     public static void shutdown()
     {
