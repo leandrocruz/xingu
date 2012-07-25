2ffbe67a11b77c8998fc66c3b4008a2e2cc48616
Leandro Cruz <leandro.saad@gmail.com>
2012-07-25 18:57:30 -0300

    NA

---
+++ exception/api/src/main/java/xingu/exception/impl/SimpleExceptionHandler.java
@@ -0,0 +1,16 @@
+package xingu.exception.impl;
+
+import xingu.exception.ExceptionHandler;
+
+public class SimpleExceptionHandler
+	implements ExceptionHandler
+{
+
+	@Override
+	public void handle(Throwable throwable)
+	{}
+
+	@Override
+	public void handle(Throwable throwable, Thread thread)
+	{}
+}
