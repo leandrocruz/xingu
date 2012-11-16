1ee565a5ac3964b52cf12b58d5ab4558dbae5062
Leandro Cruz <leandro.saad@gmail.com>
2012-11-16 15:37:30 -0200

    Added default impl for some components

--- email/simple/src/main/java/xingu/email/impl/EmailManagerImpl.java
+++ email/simple/src/main/java/xingu/email/impl/EmailManagerImpl.java
@@ -1,59 +1,29 @@
 package xingu.email.impl;
 
-import javax.mail.Address;
-import javax.mail.Message;
-import javax.mail.Message.RecipientType;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeUtility;
-
+import br.com.ibnetwork.xingu.lang.NotImplementedYet;
 import xingu.email.Email;
-import br.com.ibnetwork.xingu.container.Inject;
-import br.com.ibnetwork.xingu.messaging.MessageDispatcher;
+import xingu.email.EmailManager;
 
 public class EmailManagerImpl
-    extends EmailManagerSupport
+	implements EmailManager
 {
-    @Inject
-    private MessageDispatcher messageDispatcher;
 
-    protected boolean send(Email email)
-        throws Exception
-    {
-        if (!deliveryEnabled)
-        {
-            logger.warn("Email delivery is not enabled!");
-            return false;
-        }
+	@Override
+	public void sendMessage(Email email)
+		throws Exception
+	{
+		throw new NotImplementedYet();
+	}
 
-        try
-        {
-            Message message = createMessageFrom(email);
-            return messageDispatcher.sendMessage(message);
-        }
-        catch (Throwable t)
-        {
-            logger.error("Error sending message ", t);
-            return false;
-        }
-    }
+	@Override
+	public boolean isEmailSent(String type, String to)
+	{
+		throw new NotImplementedYet();
+	}
 
-    protected Message createMessageFrom(Email email)
-        throws Exception
-    {
-        Address to = toAddress(email.getToName(), email.getToAddress());
-        Address from = toAddress(email.getFromName(), email.getFromAddress());
-        String subject = email.getSubject();
-        Message message = new MimeMessage(messageDispatcher.getSession());
-        message.setFrom(from);
-        message.setRecipient(RecipientType.TO, to);
-        message.setSubject(MimeUtility.encodeText(subject));
-
-        String html = render(email, email.getHtmlTemplate(), email.getHtmlLayoutTemplate());
-        
-        //TODO: use multipart and add the text content 
-        String txt = render(email, email.getTextTemplate(), email.getTextLayoutTemplate());
-        
-        message.setContent(html, "text/html; charset=utf-8");
-        return message;
-    }
+	@Override
+	public Email findLastEmailSent(String to, String typeLike)
+	{
+		throw new NotImplementedYet();
+	}
 }
---
+++ email/simple/src/main/java/xingu/email/impl/SimpleEmailManager.java
@@ -0,0 +1,59 @@
+package xingu.email.impl;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.Message.RecipientType;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeUtility;
+
+import xingu.email.Email;
+import br.com.ibnetwork.xingu.container.Inject;
+import br.com.ibnetwork.xingu.messaging.MessageDispatcher;
+
+public class SimpleEmailManager
+    extends EmailManagerSupport
+{
+    @Inject
+    private MessageDispatcher messageDispatcher;
+
+    protected boolean send(Email email)
+        throws Exception
+    {
+        if (!deliveryEnabled)
+        {
+            logger.warn("Email delivery is not enabled!");
+            return false;
+        }
+
+        try
+        {
+            Message message = createMessageFrom(email);
+            return messageDispatcher.sendMessage(message);
+        }
+        catch (Throwable t)
+        {
+            logger.error("Error sending message ", t);
+            return false;
+        }
+    }
+
+    protected Message createMessageFrom(Email email)
+        throws Exception
+    {
+        Address to = toAddress(email.getToName(), email.getToAddress());
+        Address from = toAddress(email.getFromName(), email.getFromAddress());
+        String subject = email.getSubject();
+        Message message = new MimeMessage(messageDispatcher.getSession());
+        message.setFrom(from);
+        message.setRecipient(RecipientType.TO, to);
+        message.setSubject(MimeUtility.encodeText(subject));
+
+        String html = render(email, email.getHtmlTemplate(), email.getHtmlLayoutTemplate());
+        
+        //TODO: use multipart and add the text content 
+        String txt = render(email, email.getTextTemplate(), email.getTextLayoutTemplate());
+        
+        message.setContent(html, "text/html; charset=utf-8");
+        return message;
+    }
+}
