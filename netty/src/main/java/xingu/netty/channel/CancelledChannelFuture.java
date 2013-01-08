167ffe5790457bf0a6f84e886a593125f7f978df
Leandro Cruz <leandro.saad@gmail.com>
2013-01-08 16:23:11 -0200

    Added CancelledChannelFuture

---
+++ netty/src/main/java/xingu/netty/channel/CancelledChannelFuture.java
@@ -0,0 +1,89 @@
+package xingu.netty.channel;
+
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelException;
+import org.jboss.netty.channel.ChannelFuture;
+import org.jboss.netty.channel.CompleteChannelFuture;
+
+public class CancelledChannelFuture
+	extends CompleteChannelFuture
+{
+	private final Throwable cause;
+
+	public CancelledChannelFuture(Channel channel, Throwable cause)
+	{
+		super(channel);
+		this.cause = cause;
+	}
+
+	@Override
+	public boolean isSuccess()
+	{
+		return false;
+	}
+	
+	@Override
+	public boolean isCancelled()
+	{
+		return true;
+	}
+
+    public boolean isDone()
+    {
+        return false;
+    }
+
+	@Override
+	public Throwable getCause()
+	{
+		return cause;
+	}
+
+	@Override
+	public ChannelFuture rethrowIfFailed()
+		throws Exception
+	{
+		if (cause instanceof Exception)
+		{
+			throw (Exception) cause;
+		}
+
+		if (cause instanceof Error)
+		{
+			throw (Error) cause;
+		}
+
+		throw new RuntimeException(cause);
+	}
+
+	@Override
+	public ChannelFuture sync()
+		throws InterruptedException
+	{
+		rethrow();
+		return this;
+	}
+
+	private void rethrow()
+	{
+		if (cause instanceof RuntimeException)
+		{
+			throw (RuntimeException) cause;
+		}
+
+		if (cause instanceof Error)
+		{
+			throw (Error) cause;
+		}
+
+		throw new ChannelException(cause);
+
+	}
+
+	@Override
+	public ChannelFuture syncUninterruptibly()
+	{
+		rethrow();
+		return this;
+	}
+}
