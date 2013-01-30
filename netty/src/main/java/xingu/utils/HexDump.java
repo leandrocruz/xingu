a3a704bfb4c334f635c785d548383a422fbf62a6
Leandro Cruz <leandro.saad@gmail.com>
2013-01-30 14:24:36 -0200

    Added toHex(byte[] bytes)

--- netty/src/main/java/xingu/utils/HexDump.java
+++ netty/src/main/java/xingu/utils/HexDump.java
@@ -13,6 +13,8 @@ public class HexDump
 	private static final String[] BYTEPADDING = new String[16];
 
 	private static final char[] BYTE2CHAR = new char[256];
+	
+	private static final char[] hexArray = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 
 	static
 	{
@@ -79,6 +81,19 @@ public class HexDump
 		}
 	}
 
+	public static String toHex(byte[] bytes)
+	{
+		char[] hexChars = new char[bytes.length * 2];
+		int v;
+		for (int j = 0; j < bytes.length; j++)
+		{
+			v = bytes[j] & 0xFF;
+			hexChars[j * 2] = hexArray[v >>> 4];
+			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+		}
+		return new String(hexChars);
+	}
+	
 	public static String toHex(ChannelBuffer buffer)
 	{
 		int length = buffer.readableBytes();
