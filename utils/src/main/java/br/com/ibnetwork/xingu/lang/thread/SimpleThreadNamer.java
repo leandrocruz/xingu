f752bcbf4b2d91329e14082c5233919dec99e515
Leandro Cruz <leandro.saad@gmail.com>
2013-04-05 09:27:13 -0300

    Added SimpleThreadNamer

---
+++ utils/src/main/java/br/com/ibnetwork/xingu/lang/thread/SimpleThreadNamer.java
@@ -0,0 +1,18 @@
+package br.com.ibnetwork.xingu.lang.thread;
+
+public class SimpleThreadNamer
+	implements ThreadNamer
+{
+	private final String prefix;
+
+	public SimpleThreadNamer(String prefix)
+	{
+		this.prefix = prefix;
+	}
+	
+	@Override
+	public String nameFor(int threadNumber)
+	{
+		return prefix + " #" + threadNumber;
+	}
+}
