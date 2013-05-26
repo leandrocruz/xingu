fd69a6659feb5408920a4e7e9f80b5f5477a13a7
Leandro Cruz <leandro.saad@gmail.com>
2013-05-25 23:36:08 -0300

    Handling exceptions when executing commands on remote console

--- node/console/src/main/java/xingu/node/console/impl/CommandTask.java
+++ node/console/src/main/java/xingu/node/console/impl/CommandTask.java
@@ -5,19 +5,24 @@ import java.util.concurrent.Callable;
 import xingu.codec.Codec;
 import xingu.node.console.command.Command;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.jboss.netty.channel.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CommandTask
 	implements Callable<Command>
 {
 
-	private final Channel channel;
-	
-	private final Command command;
-	
-	private final String[] args;
+	private final Channel	channel;
 
-	private final Codec codec;
+	private final Command	command;
+
+	private final String[]	args;
+
+	private final Codec		codec;
+
+	private Logger			logger	= LoggerFactory.getLogger(getClass());
 
 	public CommandTask(Codec codec, Channel channel, Command command, String[] args)
 	{
@@ -31,8 +36,18 @@ public class CommandTask
 	public Command call()
 		throws Exception
 	{
-		String text = "OK";
-		Object result = command.execute(args);
+		String text   = "OK";
+		Object result = null;
+		try
+		{
+			result = command.execute(args);
+		}
+		catch(Throwable t)
+		{
+			logger.error("Error executing command '" + command + "'", t);
+			text = ExceptionUtils.getStackTrace(t);
+		}
+		
 		if(result != null)
 		{
 			if(result instanceof String)
