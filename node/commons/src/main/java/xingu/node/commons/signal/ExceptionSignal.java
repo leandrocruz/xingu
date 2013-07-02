b00abcf2ae2a1de6a9a4af3cf999174130a64439
Leandro Cruz <leandro.saad@gmail.com>
2013-07-02 14:39:23 -0300

    Priting exception trace

--- node/commons/src/main/java/xingu/node/commons/signal/ExceptionSignal.java
+++ node/commons/src/main/java/xingu/node/commons/signal/ExceptionSignal.java
@@ -1,11 +1,12 @@
 package xingu.node.commons.signal;
 
+import xingu.node.commons.signal.behavior.SignalBehavior;
+
 public class ExceptionSignal
 	extends SignalSupport
+	implements SignalBehavior<ExceptionSignal, BooleanSignal>
 {
 	private Signal		signal;
-
-	private Throwable	error;
 
 	private String		trace;
 
@@ -18,16 +19,17 @@ public class ExceptionSignal
 		this.trace  = trace;
 	}
 
-	public ExceptionSignal(Signal signal, Throwable t)
-	{
-		this.signal = signal;
-		this.error  = t;
-	}
-
 	public Signal getSignal(){return signal;}
 	public void setSignal(Signal signal){this.signal = signal;}
-	public Throwable getError(){return error;}
-	public void setError(Throwable error){this.error = error;}
 	public String getTrace(){return trace;}
 	public void setTrace(String trace){this.trace = trace;}
+
+	@Override
+	public BooleanSignal perform(ExceptionSignal signal)
+		throws Exception
+	{
+		System.err.println("Error Executing Signal " + this.signal);
+		System.err.println(trace);
+		return null;
+	}
 }
