f87ce983c47fc2a37abb1b2f8c99de82de69a4f3
Leandro Cruz <leandro.saad@gmail.com>
2013-08-28 15:23:53 -0300

    Added new package with Ping class

---
+++ utils/src/main/java/xingu/utils/network/PingResult.java
@@ -0,0 +1,67 @@
+package xingu.utils.network;
+
+public class PingResult
+{
+
+	private int	transmitted;
+
+	private int	received;
+
+	private int	loss;
+
+	private int	time;
+
+	private int	errors;
+
+	public PingResult(int transmitted, int received, int loss, int time, int errors)
+	{
+		this.transmitted = transmitted;
+		this.received    = received;
+		this.loss        = loss;
+		this.time        = time;
+		this.errors      = errors;
+	}
+
+	public int getTransmitted()
+	{
+		return transmitted;
+	}
+
+	public int getReceived()
+	{
+		return received;
+	}
+
+	public int getLoss()
+	{
+		return loss;
+	}
+
+	public int getTime()
+	{
+		return time;
+	}
+
+	public int getErrors()
+	{
+		return errors;
+	}
+
+	@Override
+	public String toString()
+	{
+		String err = "";
+		if(errors > 0)
+		{
+			err = "+" + errors + " errors, ";
+		}
+		return
+				transmitted + " packets transmitted, " 
+				+ received + " received, "
+				+ err
+				+ loss + "% packet loss, "
+				+ "time " + time + "ms";
+	}
+	
+	
+}
---
+++ utils/src/main/java/xingu/utils/network/Pinger.java
@@ -0,0 +1,72 @@
+package xingu.utils.network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+
+public class Pinger
+{
+	private Pattern pattern = Pattern.compile("(\\d+) packets transmitted, (\\d+) received,( \\+(\\d+) errors,)? (\\d+)% packet loss, time (\\d+)ms");
+
+	public static final void main(String[] args)
+		throws Exception
+	{
+		String addr = "192.168.0.1";
+		if(args.length > 0)
+		{
+			addr = args[0];
+		}
+		PingResult result = new Pinger().ping(addr);
+		System.out.println(result);
+	}
+
+	public PingResult ping(String addr)
+		throws IOException
+	{
+		ProcessBuilder builder   = new ProcessBuilder("ping", "-c3", addr);
+		builder.redirectErrorStream(true);
+		Process        process   = builder.start();
+		InputStream    is        = process.getInputStream();
+		BufferedReader reader    = new BufferedReader(new InputStreamReader(is));
+
+		String line;
+		PingResult   result = null;
+		boolean      flag   = false;
+		while((line = reader.readLine()) != null)
+		{
+			//System.err.println(line);
+			if(!flag && line.startsWith("---"))
+			{
+				flag = true;
+				continue;
+			}
+			if(flag)
+			{
+				Matcher matcher = pattern.matcher(line);
+				if(matcher.matches())
+				{
+					int    transmitted = Integer.parseInt(matcher.group(1));
+					int    received    = Integer.parseInt(matcher.group(2));
+					int    loss        = Integer.parseInt(matcher.group(5));
+					int    time        = Integer.parseInt(matcher.group(6));
+
+					String err    = matcher.group(4);
+					int    errors = StringUtils.isEmpty(err) ? 0 : Integer.parseInt(err);
+
+					IOUtils.closeQuietly(reader);
+					return new PingResult(transmitted, received, loss, time, errors);
+				}
+			}
+		}
+
+		IOUtils.closeQuietly(reader);
+		
+		return result;
+	}
+}
