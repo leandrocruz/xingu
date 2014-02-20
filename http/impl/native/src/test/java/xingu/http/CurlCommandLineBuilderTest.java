8f7a114716e7a2716f89138e74e96a42923159de
Leandro Cruz <leandro.saad@gmail.com>
2014-02-20 16:13:29 -0300

    More Http tests

--- http/impl/native/src/test/java/xingu/http/CurlCommandLineBuilderTest.java
+++ http/impl/native/src/test/java/xingu/http/CurlCommandLineBuilderTest.java
@@ -2,13 +2,16 @@ package xingu.http;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import java.io.File;
-import java.net.URL;
-
+import org.jboss.netty.handler.codec.http.Cookie;
 import org.jboss.netty.handler.codec.http.HttpHeaders;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.junit.Test;
 
+import xingu.http.client.CookieUtils;
+import xingu.http.client.Cookies;
 import xingu.http.client.HttpRequest;
 import xingu.http.client.HttpResponse;
 import xingu.http.client.impl.CommandLineBuilder;
@@ -35,8 +38,7 @@ public class CurlCommandLineBuilderTest
 		throws Exception
 	{
 		HttpRequest  req      = mock(HttpRequest.class);
-		URL          url      = getResource("response1.txt");
-		HttpResponse response = builder.responseFrom(req, new File(url.getFile()));
+		HttpResponse response = builder.responseFrom(req, getFile("response1.txt"));
 		assertEquals(200, 		response.getCode());
 		assertEquals("1000", 	response.getHeader(HttpHeaders.Names.CONTENT_LENGTH));
 		assertEquals("Jetty(8.1.2.v20120308)", response.getHeader("Server"));
@@ -47,9 +49,35 @@ public class CurlCommandLineBuilderTest
 		throws Exception
 	{
 		HttpRequest req = mock(HttpRequest.class);
-		URL         url = getResource("response2.txt");
-		builder.responseFrom(req, new File(url.getFile()));
+		builder.responseFrom(req, getFile("response2.txt"));
 		throw new Exception("Should have thrown exception");
 	}
 
+	@Test
+	public void testHttp200Response()
+		throws Exception
+	{
+		HttpRequest req = mock(HttpRequest.class);
+		when(req.getUri()).thenReturn("www.globo.com");
+		HttpResponse response = builder.responseFrom(req, getFile("response3.txt"));
+		Document     doc      = response.getDocument();
+		Element      el       = doc.getElementById("busca-padrao");
+		assertEquals(200, response.getCode());
+		assertEquals("buscarbuscarbuscar", el.text());
+	}
+
+	@Test
+	public void testHttpCookies()
+		throws Exception
+	{
+		HttpRequest req = mock(HttpRequest.class);
+		when(req.getUri()).thenReturn("www.google.com");
+		HttpResponse response = builder.responseFrom(req, getFile("response4.txt"));
+		Cookies      cookies  = CookieUtils.getCookies(response);
+		Cookie       c1       = cookies.byName("PREF");
+		Cookie       c2       = cookies.byName("NID");
+		assertEquals(302, response.getCode());
+		assertEquals("ID=ce20a24454b3e7b0:FF=0:TM=1392923056:LM=1392923056:S=XJzMcL5FMR_wGaFN", c1.getValue());
+		assertEquals("67=gbVYO3CBYtd5fAHBUEPbz6FkA8vQ7MqymZaWefWUgAK0gfW0PGnZc7ByFguhcOI798Kh-y6eZVNaqYJVZ2ytjGl7Kbsvx21ndYBds0qUkS-NsBeBRuBMh3mcvR55N18S", c2.getValue());
+	}
 }
