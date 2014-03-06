6fa67e4f84acd8cd4ffb7468e7d6af8404d91b60
Leandro Cruz <leandro.saad@gmail.com>
2014-03-06 09:41:54 -0300

    Added @ToMap and @ToString annotations to guide the encoding of pojos at PojoEncoder

--- node/commons/src/main/java/xingu/node/commons/protocol/PojoEncoder.java
+++ node/commons/src/main/java/xingu/node/commons/protocol/PojoEncoder.java
@@ -1,11 +1,17 @@
 package xingu.node.commons.protocol;
 
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.beanutils.BeanUtils;
 import org.jboss.netty.channel.Channel;
 import org.jboss.netty.util.CharsetUtil;
 
-import br.com.ibnetwork.xingu.container.Inject;
 import xingu.codec.Codec;
 import xingu.netty.protocol.FrameBasedMessageEncoder;
+import br.com.ibnetwork.xingu.container.Inject;
+import br.com.ibnetwork.xingu.utils.FieldUtils;
 
 public class PojoEncoder
 	extends FrameBasedMessageEncoder
@@ -17,7 +23,57 @@ public class PojoEncoder
 	protected byte[] toByteArray(Channel channel, Object obj, int type)
 		throws Exception
 	{
+        Class<?>    clazz  = obj.getClass();
+		List<Field> fields = FieldUtils.getAllFields(clazz);
+        for (Field field : fields)
+        {
+            processInject(obj, field);
+            processToString(obj, field);
+            processToMap(obj, field);
+        }
+
     	String encoded = codec.encode(obj);
     	return encoded.getBytes(CharsetUtil.UTF_8);
+	}
+
+	private void processToMap(Object obj, Field field)
+		throws Exception
+	{
+		Class<?> clazz = obj.getClass();
+		ToMap    toMap = field.getAnnotation(ToMap.class);
+		if(toMap != null)
+		{
+			String storeTo = toMap.value();
+			Field  target  = FieldUtils.getField(clazz, storeTo);
+			Object value   = FieldUtils.valueFrom(field, obj);
+			Map    map     = BeanUtils.describe(value);
+			FieldUtils.set(target, obj, map);
+			FieldUtils.set(field,  obj, null);
+		}
+	}
+
+	private void processToString(Object obj, Field field)
+		throws Exception
+	{
+		Class<?> clazz    = obj.getClass();
+		ToString toString = field.getAnnotation(ToString.class);
+		if(toString != null)
+		{
+			String storeTo = toString.value();
+			Field  target  = FieldUtils.getField(clazz, storeTo);
+			Object value   = FieldUtils.valueFrom(field, obj);
+			String text    = codec.encode(value);
+			FieldUtils.set(target, obj, text);
+			FieldUtils.set(field,  obj, null);
+		}
+	}
+
+	private void processInject(Object obj, Field field)
+	{
+		Inject inject = field.getAnnotation(Inject.class);
+		if(inject != null)
+		{
+			FieldUtils.set(field, obj, null);
+		}
 	}
 }
