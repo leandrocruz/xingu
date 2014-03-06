876fdd68f0253e1ae21cc6db29eb2550f33b4e6d
Leandro Cruz <leandro.saad@gmail.com>
2014-03-05 21:00:21 -0300

    Added SimpleCodec

---
+++ codec/simple/src/main/java/xingu/codec/simple/SimpleCodec.java
@@ -0,0 +1,47 @@
+package xingu.codec.simple;
+
+import java.io.StringWriter;
+
+import org.apache.avalon.framework.activity.Initializable;
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.core.Persister;
+
+import br.com.ibnetwork.xingu.lang.NotImplementedYet;
+import xingu.codec.Codec;
+
+public class SimpleCodec
+	implements Codec, Initializable
+{
+
+	private Serializer	serializer;
+
+	@Override
+	public void initialize()
+		throws Exception
+	{
+		serializer = new Persister();
+	}
+
+	@Override
+	public Object decode(String text)
+		throws Exception
+	{
+		throw new NotImplementedYet();
+	}
+
+	@Override
+	public <T> T decode(String text, Class<? extends T> clazz)
+		throws Exception
+	{
+		return serializer.read(clazz, text);
+	}
+
+	@Override
+	public String encode(Object object)
+		throws Exception
+	{
+		StringWriter writer = new StringWriter();
+		serializer.write(object, writer);
+		return writer.toString();
+	}
+}
