5f53a73a64c5d041b9c3eec6ff746b4546634954
Leandro Cruz <leandro.saad@gmail.com>
2014-04-07 17:19:59 -0300

    Printing exceptions

--- node/commons/src/main/java/xingu/node/commons/signal/impl/ExceptionCaught.java
+++ node/commons/src/main/java/xingu/node/commons/signal/impl/ExceptionCaught.java
@@ -2,8 +2,12 @@ package xingu.node.commons.signal.impl;
 
 import org.jboss.netty.channel.Channel;
 
+import xingu.node.commons.signal.Signal;
+import xingu.node.commons.signal.behavior.SignalBehavior;
+
 public class ExceptionCaught
 	extends SignalSupport
+	implements SignalBehavior<ExceptionCaught, Signal>
 {
 	private Channel		channel;
 
@@ -19,4 +23,12 @@ public class ExceptionCaught
 	public void setChannel(Channel channel){this.channel = channel;}
 	public Throwable getCause(){return cause;}
 	public void setCause(Throwable cause){this.cause = cause;}
+
+	@Override
+	public Signal perform(ExceptionCaught signal, Channel channel)
+		throws Exception
+	{
+		cause.printStackTrace();
+		return null;
+	}
 }
