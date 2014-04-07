386aa81099f9682a33b516a29d7db9867f4e3e36
Leandro Cruz <leandro.saad@gmail.com>
2014-04-07 10:51:53 -0300

    Added simple impls of pojo encoder/decoder

---
+++ node/commons/src/main/java/xingu/node/commons/protocol/impl/SimplePojoDecoder.java
@@ -0,0 +1,23 @@
+package xingu.node.commons.protocol.impl;
+
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.util.CharsetUtil;
+
+import xingu.codec.Codec;
+import xingu.netty.protocol.FrameBasedMessageDecoder;
+import br.com.ibnetwork.xingu.container.Inject;
+
+public class SimplePojoDecoder
+	extends FrameBasedMessageDecoder
+{
+	@Inject("proto")
+	private Codec	codec;
+
+	@Override
+	protected Object toObject(Channel channel, int type, byte[] data)
+		throws Exception
+	{
+		String input = new String(data, CharsetUtil.UTF_8);
+		return codec.decode(input);
+	}
+}
