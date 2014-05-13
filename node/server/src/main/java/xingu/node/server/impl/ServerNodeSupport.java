5c12e7c78fb520de838e4fb336ea1d268ea96a74
Leandro Cruz <leandro.saad@gmail.com>
2014-05-13 13:44:55 -0300

    Import Cleanup

--- node/server/src/main/java/xingu/node/server/impl/ServerNodeSupport.java
+++ node/server/src/main/java/xingu/node/server/impl/ServerNodeSupport.java
@@ -3,7 +3,6 @@ package xingu.node.server.impl;
 import static org.jboss.netty.channel.Channels.pipeline;
 
 import java.net.InetSocketAddress;
-import java.util.Iterator;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
