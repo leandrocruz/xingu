7731aba49454c90ae455e9b60367717fd5fa8bd8
Leandro Cruz <leandro.saad@gmail.com>
2014-05-21 11:39:04 -0300

    Added xingu-xls

---
+++ xls/src/main/java/xingu/xls/ExcelReader.java
@@ -0,0 +1,71 @@
+package xingu.xls;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+public class ExcelReader
+{
+	public static void main(String[] args)
+		throws Exception
+	{
+		String file  = args[0];
+		String count = args[1];
+		List<Record> records = ExcelReader.read(file, Integer.valueOf(count), 0);
+		
+		int i = 0;
+		for(Record record : records)
+		{
+			System.out.println((i++) + ". " + record.get("NOME") + " ("+record.get("DOCUMENTO")+") em " + record.get("COMARCA"));
+		}
+	}
+
+	public static List<Record> read(String fileName, int cols, int sheetNumber)
+		throws Exception
+	{
+		List<Record> result = new ArrayList<Record>();
+		InputStream  is     = new FileInputStream(fileName);
+		Workbook     wb     = new HSSFWorkbook(is);
+		Sheet        sheet  = wb.getSheetAt(sheetNumber);
+		
+		String[] meta = new String[cols];
+		int i = 0;
+		for(Row row : sheet)
+		{
+			if(i == 0)
+			{
+				for(int j = 0; j < cols; j++)
+				{
+					Cell    cell  = row.getCell(j);
+					if(cell == null)
+					{
+						throw new Exception("Row '"+(j+1)+"' is  null. Maybe you should check the 'cols' argument");
+					}
+					String  value = cell.getStringCellValue();
+					meta[j]       = value.trim();
+				}				
+			}
+			else
+			{
+				Record record = new Record();
+				for(int j = 0; j < cols; j++)
+				{
+					Cell   cell   = row.getCell(j);
+					String value  = cell.getStringCellValue();
+					String name   = meta[j];
+					record.add(name, value.trim());
+				}
+				result.add(record);
+			}
+			i++;
+		}
+		return result;
+	}
+}
---
+++ xls/src/main/java/xingu/xls/Record.java
@@ -0,0 +1,19 @@
+package xingu.xls;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Record
+{
+	private Map<String, String> map = new HashMap<String, String>();
+	
+	public String get(String name)
+	{
+		return map.get(name);
+	}
+
+	public void add(String name, String value)
+	{
+		map.put(name, value);
+	}
+}
