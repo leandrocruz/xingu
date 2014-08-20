6d8d61649e72740219175f1046d106f2d6b6e0eb
Leandro Cruz <leandro.saad@gmail.com>
2014-08-20 18:29:27 -0300

    Printing details

--- servlet/utils/src/main/java/xingu/servlet/utils/XinguUploadServlet.java
+++ servlet/utils/src/main/java/xingu/servlet/utils/XinguUploadServlet.java
@@ -1,8 +1,12 @@
 package xingu.servlet.utils;
 
 import java.io.File;
+import java.io.InputStream;
 import java.io.PrintStream;
+import java.net.URL;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -32,42 +36,84 @@ public class XinguUploadServlet
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, java.io.IOException 
 	{
-		System.out.println("CHARSET> " + request.getCharacterEncoding());
-		
-		boolean isMultipart = ServletFileUpload.isMultipartContent(request);
-		if(!isMultipart)
-		{
-			ServletOutputStream os = response.getOutputStream();
-			IOUtils.write("Not a file upload", os);
-			os.close();
-			return;
-		}
-		
-		DiskFileItemFactory factory = new DiskFileItemFactory();
-		factory.setSizeThreshold(max);
-		factory.setRepository(repo);
-		ServletFileUpload upload = new ServletFileUpload(factory);
-		upload.setSizeMax(max);
-
 		try
 		{
+			String result = process(request, response);
+			response.addHeader("Content-Type:", "text/html; charset=utf8");
+			ServletOutputStream os = response.getOutputStream();
+			IOUtils.write(result, os);
+			os.close();
+		}
+		catch(Throwable t)
+		{
+			t.printStackTrace();
+		}
+	}
+
+	private String process(HttpServletRequest request, HttpServletResponse response)
+		throws Exception
+	{
+		InputStream  is   = Thread.currentThread().getContextClassLoader().getResourceAsStream("form.html");
+		String       form = IOUtils.toString(is);
+		StringBuffer sb   = new StringBuffer(form);
+		
+		sb.append("<h4>Headers</h4>");
+		Enumeration<String> names = request.getHeaderNames();
+		while(names.hasMoreElements())
+		{
+			String name = names.nextElement();
+			Enumeration<String> headers = request.getHeaders(name);
+			while(headers.hasMoreElements())
+			{
+				String header = (String) headers.nextElement();
+				sb.append(name).append(" = [").append(header).append("]<br/>\n");
+			}
+		}
+
+		sb.append("<h4>Info</h4>\n");
+		boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+
+		sb.append("CHARSET: ").append(request.getCharacterEncoding()).append("<br/>\n");
+		sb.append("isMultipart: ").append(isMultipart).append("<br/>\n");
+
+		Map<String, String[]> params = request.getParameterMap();
+		sb.append("<h4>Params</h4>\n");
+		for(String name : params.keySet())
+		{
+			String[] values = params.get(name);
+			for(String value : values)
+			{
+				sb.append(name).append(" = [").append(value).append("]<br/>\n");
+			}
+		}
+		
+		if(isMultipart)
+		{
+			sb.append("<h4>Upload</h4>\n");
+			DiskFileItemFactory factory = new DiskFileItemFactory();
+			factory.setSizeThreshold(max);
+			factory.setRepository(repo);
+			ServletFileUpload upload = new ServletFileUpload(factory);
+			upload.setSizeMax(max);
+
 			List<FileItem> fileItems = upload.parseRequest(request);
 			for(FileItem fi : fileItems)
 			{
 				boolean formField = fi.isFormField();
+				sb.append(fi.getFieldName()).append(" = [");
+				String value;
 				if(formField)
 				{
-					System.out.println(fi.getFieldName() + "=" + fi.getString("utf-8"));
+					value = fi.getString("utf-8");
 				}
 				else
 				{
-					System.out.println(fi.getFieldName() + " (" + fi.getSize()+")");
+					value = fi.getFieldName() + " (" + fi.getSize() + ")";
 				}
+				sb.append(value).append("]<br/>\n");
 			}
 		}
-		catch(FileUploadException e)
-		{
-			e.printStackTrace();
-		}
+		
+		return sb.toString();
 	}
 }
