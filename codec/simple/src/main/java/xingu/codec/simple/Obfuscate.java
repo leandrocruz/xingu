cf657ea06922aaffacd665bbbaad62cc454e7e84
Leandro Cruz <leandro.saad@gmail.com>
2014-09-01 12:35:02 -0300

    Using AnnotationStrategy. Added Obfuscate converter

---
+++ codec/simple/src/main/java/xingu/codec/simple/Obfuscate.java
@@ -0,0 +1,27 @@
+package xingu.codec.simple;
+
+import org.apache.commons.lang3.StringUtils;
+import org.simpleframework.xml.convert.Converter;
+import org.simpleframework.xml.stream.InputNode;
+import org.simpleframework.xml.stream.OutputNode;
+
+import br.com.ibnetwork.xingu.lang.NotImplementedYet;
+
+public class Obfuscate
+	implements Converter<String>
+{
+	@Override
+	public String read(InputNode node)
+		throws Exception
+	{
+		throw new NotImplementedYet();
+	}
+
+	@Override
+	public void write(OutputNode node, String value)
+		throws Exception
+	{
+		String replacement = StringUtils.repeat("*", value.length());
+		node.setValue(replacement);
+	}
+}
