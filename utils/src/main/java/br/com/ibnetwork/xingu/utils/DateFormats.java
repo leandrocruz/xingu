92fdc24c63a779eb65e1662bc5698a5cd0a77bfd
Leandro Cruz <leandro.saad@gmail.com>
2014-09-15 15:13:11 -0300

    Added more date formats

--- utils/src/main/java/br/com/ibnetwork/xingu/utils/DateFormats.java
+++ utils/src/main/java/br/com/ibnetwork/xingu/utils/DateFormats.java
@@ -6,4 +6,8 @@ import java.text.SimpleDateFormat;
 public class DateFormats
 {
 	public static final DateFormat yyyyMMdd_HHmmss = new SimpleDateFormat("yyyyMMdd.HHmmss");
+	
+	public static final DateFormat ddMMyyyy = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public static final DateFormat MMyyyy = new SimpleDateFormat("MM/yyyy");
 }
