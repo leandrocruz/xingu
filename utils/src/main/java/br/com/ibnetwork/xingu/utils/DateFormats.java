dc61a824fe5bce0a5bb8db17d180c78c1a60e40e
Leandro Cruz <leandro.saad@gmail.com>
2014-09-30 16:14:01 -0300

    Replacing SimpleDateFormat by FastDateFormat

--- utils/src/main/java/br/com/ibnetwork/xingu/utils/DateFormats.java
+++ utils/src/main/java/br/com/ibnetwork/xingu/utils/DateFormats.java
@@ -1,13 +1,12 @@
 package br.com.ibnetwork.xingu.utils;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import org.apache.commons.lang3.time.FastDateFormat;
 
 public class DateFormats
 {
-	public static final DateFormat yyyyMMdd_HHmmss = new SimpleDateFormat("yyyyMMdd.HHmmss");
-	
-	public static final DateFormat ddMMyyyy = new SimpleDateFormat("dd/MM/yyyy");
-	
-	public static final DateFormat MMyyyy = new SimpleDateFormat("MM/yyyy");
+	public static final FastDateFormat yyyyMMdd_HHmmss = FastDateFormat.getInstance("yyyyMMdd.HHmmss");
+
+	public static final FastDateFormat ddMMyyyy        = FastDateFormat.getInstance("dd/MM/yyyy");
+
+	public static final FastDateFormat MMyyyy          = FastDateFormat.getInstance("MM/yyyy");
 }
