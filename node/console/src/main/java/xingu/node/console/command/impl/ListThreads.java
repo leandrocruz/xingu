4f020dc7c76d6c21afbb42964586a5f71b63dc9e
Leandro Cruz <leandro.saad@gmail.com>
2014-11-20 12:58:33 -0200

    Added filtering options for ListThreads command

--- node/console/src/main/java/xingu/node/console/command/impl/ListThreads.java
+++ node/console/src/main/java/xingu/node/console/command/impl/ListThreads.java
@@ -1,9 +1,12 @@
 package xingu.node.console.command.impl;
 
+import java.lang.Thread.State;
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
 import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
 
 import xingu.node.console.command.Command;
 import xingu.node.console.command.Writer;
@@ -15,6 +18,23 @@ public class ListThreads
 	public String execute(String[] args, Writer writer)
 		throws Exception
 	{
+		String nameToMach   = null;
+		String stateToMatch = null;
+		if(args != null && args.length > 0)
+		{
+			for(String arg : args)
+			{
+				if(arg.startsWith("n:"))
+				{
+					nameToMach = arg.substring(2);
+				}
+				else if(arg.startsWith("s:"))
+				{
+					stateToMatch = arg.substring(2);
+				}
+			}
+		}
+
 		Map<Thread, StackTraceElement[]> map = Thread.getAllStackTraces();
 		
 		ThreadMXBean manager = ManagementFactory.getThreadMXBean();
@@ -34,12 +54,29 @@ public class ListThreads
 				}
 			}
 
-			sb.append(id).append("\t")
-				.append(thread.isDaemon()).append("\t")
-				.append(info.isSuspended()).append("\t\t")
-				.append(info.getThreadState()).append(" \t")
-				.append(info.getThreadName())
-				.append("\n");
+			String name = info.getThreadName();
+			boolean include = true;
+			if(nameToMach != null)
+			{
+				include = StringUtils.indexOfIgnoreCase(name, nameToMach) >= 0;
+			}
+			
+			State state = info.getThreadState();
+			if(stateToMatch != null)
+			{
+				include = include && state.name().equalsIgnoreCase(stateToMatch);
+			}
+
+			if(include)
+			{
+				sb
+					.append(id).append("\t")
+					.append(thread != null ? thread.isDaemon() : "NA").append("\t")
+					.append(info.isSuspended()).append("\t\t")
+					.append(state).append(" \t")
+					.append(name)
+					.append("\n");
+			}
 		}
 		return sb.toString();
 	}
