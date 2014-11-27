200ff586a33a2979af91d909be7b94aa78e02733
Leandro Cruz <leandro.saad@gmail.com>
2014-11-27 11:11:01 -0200

    LocalSpawner nor works with more than one instance

--- cloud/api/src/main/java/xingu/cloud/meta/impl/local/LocalMetaProvider.java
+++ cloud/api/src/main/java/xingu/cloud/meta/impl/local/LocalMetaProvider.java
@@ -3,33 +3,59 @@ package xingu.cloud.meta.impl.local;
 import java.io.File;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 
-import br.com.ibnetwork.xingu.utils.StringUtils;
+import br.com.ibnetwork.xingu.lang.NotImplementedYet;
 import xingu.cloud.meta.MetaProvider;
 
 public class LocalMetaProvider
 	implements MetaProvider
 {
+	private File root = new File("/tmp/oystr/meta");
+	
 	@Override
 	public String getInstanceId()
 		throws Exception
 	{
-		File file = new File("/tmp/oystr/meta");
-		String id = FileUtils.readFileToString(file);
-		return StringUtils.trim(id);
+		File     file  = new File(root, "instance-ids");
+		String   id    = StringUtils.trim(FileUtils.readFileToString(file));
+		String[] parts = id.split(",");
+		int      len   = parts.length;
+		if(len <= 0)
+		{
+			throw new NotImplementedYet();
+		}
+		
+		id = parts[0];
+		if(len > 1)
+		{
+			//we have more ids
+			String[] remaining = new String[len - 1];
+			System.arraycopy(parts, 1, remaining, 0, len - 1);
+			FileUtils.write(file, StringUtils.join(remaining, ","));
+		}
+		else
+		{
+			FileUtils.write(file, "");
+		}
+		return id;
 	}
 
 	@Override
 	public String getHost()
 		throws Exception
 	{
-		return null;
+		String result = FileUtils.readFileToString(new File(root, "host"));
+		return StringUtils.trim(result);
 	}
 
 	@Override
 	public int[] getPorts()
 		throws Exception
 	{
-		return null;
+		String result = FileUtils.readFileToString(new File(root, "port"));
+		result = StringUtils.trim(result);
+		int port = Integer.parseInt(result);
+		return new int[]{port};
 	}
 }
--- cloud/api/src/main/java/xingu/cloud/spawner/impl/local/LocalSpawner.java
+++ cloud/api/src/main/java/xingu/cloud/spawner/impl/local/LocalSpawner.java
@@ -14,14 +14,17 @@ import xingu.cloud.spawner.impl.SpawnerSupport;
 public class LocalSpawner
 	extends SpawnerSupport
 {
+	private File root = new File("/tmp/oystr/meta");
+
 	@Override
 	protected List<Surrogate> spawn(SpawnRequest req, String... ids)
 		throws Exception
 	{
+		FileUtils.writeStringToFile(new File(root, "host"), "127.0.0.1");
+		FileUtils.writeStringToFile(new File(root, "port"), "8899");
+		
 		String all  = StringUtils.join(ids, ",");
-		File file = new File("/tmp/oystr/meta");
-		FileUtils.writeStringToFile(file, all);
-
+		FileUtils.writeStringToFile(new File(root, "instance-ids"), all);
 		List<Surrogate> result = new ArrayList<>(ids.length);
 		for(String id : ids)
 		{
