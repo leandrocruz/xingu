6b63ad1c1d90cfae9e950dbd153562d05335dd3b
Leandro Cruz <leandro.saad@gmail.com>
2014-12-10 14:02:44 -0200

    Code cleanup

--- utils/src/main/java/br/com/ibnetwork/xingu/utils/io/TreeVisitor.java
+++ utils/src/main/java/br/com/ibnetwork/xingu/utils/io/TreeVisitor.java
@@ -2,8 +2,6 @@ package br.com.ibnetwork.xingu.utils.io;
 
 import java.io.File;
 import java.io.FileFilter;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.commons.io.filefilter.TrueFileFilter;
 
@@ -13,19 +11,16 @@ public class TreeVisitor
 	
 	private final FileVisitor visitor;
 
-	private List<File> result = new ArrayList<File>();
-
 	public TreeVisitor(File root, FileVisitor visitor)
 	{
 		this.root = root;
 		this.visitor = visitor;
 	}
 
-	public List<File> visit()
+	public void visit()
 		throws Exception
 	{
 		visitRecursively(root);
-		return result;
 	}
 
 	private void visitRecursively(File start)
@@ -41,5 +36,4 @@ public class TreeVisitor
 			}
 		}
 	}
-
 }
